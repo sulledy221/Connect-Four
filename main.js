1138d8649a2598897214104c9a96c61a7bf57ae9
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-02-24 18:13:51 -0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const player1 = 
-const player2 = 
-if ()
+// Declare all state variables
+const board = [
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null]
+]; 
+// Remember render function
+
+// need eventlistner to handle click
+// function that checks for winning move
+// steps
+// 1. Handle click should update board array
+// 2. Check for a winner on the updated board array 
+// 3. Call check winner 
+// 4. Call render function, not a winner next turn 
+
+
+
+
+function checkForWinner(){
+
+}
